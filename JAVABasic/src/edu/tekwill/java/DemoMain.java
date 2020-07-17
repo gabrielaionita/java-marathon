f4d624247324faeed507c3c72aa65ff289c4513e
patrick.jane <gabriela.schendra@gmail.com>
2020-07-17 16:42:25 +0300

    -Upload exercise Lesson 3.

--- JAVABasic/src/edu/tekwill/java/DemoMain.java
+++ JAVABasic/src/edu/tekwill/java/DemoMain.java
@@ -1,13 +1,34 @@
 package edu.tekwill.java;
 
+import java.util.Scanner;
+
 public class DemoMain {
 
-    public static void main(String arg[]){
+    public static void main(String arg[]) {
 
-        String name = "Nicolae";
-        String message;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(("enter first number:"));
+        int firstNumber = scanner.nextInt();
 
-        message = "My name is " + name;
-        System.out.println (message);
+        System.out.println("Enter second number:");
+        int secondNumber = scanner.nextInt();
+
+        int biggestNumber;
+
+        if (firstNumber > secondNumber) {
+            //System.out.println("First number is bigger");
+            biggestNumber = firstNumber;.
+        }
+
+        else {
+            System.out.println("Second number is bigger");
+        }
+
+        boolean isFirstNumberBiggerThanSeconfOne = false;
+        isFirstNumberBiggerThanSeconfOne = firstNumber > secondNumber ? true : false;
+
+        System.out.println("Is first number bigger than the second one?" +isFirstNumberBiggerThanSeconfOne);
+
     }
+
 }
