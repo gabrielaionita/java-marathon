3a9a3c14c711541ef33d92d4e3acd962f1299b0e
patrick.jane <gabriela.schendra@gmail.com>
2020-07-17 16:24:21 +0300

    - Upload exercise from Lesson 3.

---
+++ JAVABasic/src/edu/tekwill/java/DemoMain.java
@@ -0,0 +1,13 @@
+package edu.tekwill.java;
+
+public class DemoMain {
+
+    public static void main(String arg[]){
+
+        String name = "Nicolae";
+        String message;
+
+        message = "My name is " + name;
+        System.out.println (message);
+    }
+}
