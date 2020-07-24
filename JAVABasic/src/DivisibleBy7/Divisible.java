ca26c03618128dfdd509c8b4f6997c448cbaf3aa
patrick.jane <gabriela.schendra@gmail.com>
2020-07-24 18:39:34 +0300

    Upload problema divisible

--- JAVABasic/src/DivisibleBy7/Divisible.java
+++ JAVABasic/src/DivisibleBy7/Divisible.java
@@ -4,17 +4,25 @@ public class Divisible {
 
     int minimum;
     int maximum;
+    int counter;
 
 
-static int Countdivisibles(int minimum, int maximum ){
+public  int Divisible(int minimum, int maximum ){
 
-    int counter = 0;
+    counter = 0;
     for( int i = minimum; i <= maximum; i++)
         if(i % 7 == 0)
             counter++;
         return counter;
+
     }
 
+    public int getCounter() {
+        return counter;
+    }
+    public void setName(String name) {
+        this.counter = counter;
+    }
 
 
 }
--- JAVABasic/src/DivisibleBy7/Main.java
+++ JAVABasic/src/DivisibleBy7/Main.java
@@ -1,24 +1,34 @@
 package DivisibleBy7;
 
+import Hexazecimal.HexazecimalChecker;
+
 import java.util.Scanner;
 
 public class Main {
 
+
+
     public static void main(String[] args) throws InvalidRangeException {
+        Divisible countDiv = new Divisible();
         Scanner scanner = new Scanner(System.in);
 
         try {
-            System.out.println("Enter a mimimum value");
+            System.out.println("Enter a mimimum value:");
             int mimimum = scanner.nextInt();
-            System.out.println("Enter a maximum value");
+            System.out.println("Enter a maximum value:");
             int maximum = scanner.nextInt();
 
+            if (maximum > mimimum) {
+
+                System.out.println("Numbers in the range divisible by 7: "  + countDiv);
+            }
             if (maximum <= mimimum)
                 throw new InvalidRangeException("Maximum number should be greater then minimum");
 
         } catch (InvalidRangeException e) {
             System.out.println("Error: " + e.getMessage());
         }
+
 
     }
 
